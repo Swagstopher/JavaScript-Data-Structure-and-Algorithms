8bafb03471adbd06124cb75bd5beca4e4ab3b9cd
Swagstopher <crystalcleare@gmail.com>
2016-04-12 18:42:54 -0700

    Added Insertion Sort

---
+++ algorithms/insertionsort.js
@@ -0,0 +1,21 @@
+/*
+Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time.
+  -Simple implementation
+  -Efficient for (quite) small data sets
+  -Worst Case: O(n^2)
+  -Best Case: O(n)
+  -Average Case: O(n^2)
+*/
+
+insertionSort: function(arr){
+  var i,j;
+
+for(i = 0; i < arr.length; i++){
+  j = i;
+  while(j>0 && arr[j-1] > arr[j] ){
+      var temp = arr[i];
+      arr[i] = arr[i-1];
+      arr[i-1] = temp;
+      j--;
+  };
+}}
